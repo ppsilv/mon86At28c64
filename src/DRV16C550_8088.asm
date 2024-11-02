181800aff8f7d2a8aa461c74b9f211514afc8184
pdsilva <ppsilv@gmail.com>
2024-11-02 16:55:09 -0300

    Serial with address 0x3F8

--- src/DRV16C550_8088.asm
+++ src/DRV16C550_8088.asm
@@ -4,17 +4,17 @@
 ; Port
 COM1:	DW		0x3F8
 ; Here are the port numbers for various UART registers:
-uart_tx_rx 		EQU  0xf8 ; 0 DLAB = 0 for Regs. TX and RX
-uart_DLL 		EQU  0xf8 ; 0 DLAB = 1 Divisor lacth low
-uart_IER 		EQU  0xf9 ; 1 DLAB = 0 Interrupt Enable Register
-uart_DLH 		EQU  0xf9 ; 1 DLAB = 1 Divisor lacth high
-uart_ISR 		EQU  0xfa ; 2 IIR Interrupt Ident. Register READ ONLY
-uart_FCR 		EQU  0xfa ; 2 Fifo Control Resgister WRITE ONLY
-uart_LCR 		EQU  0xfb ; 3 Line Control Register
-uart_MCR 		EQU  0xfc ; 4 Modem Control Register
-uart_LSR 		EQU  0xfd ; 5 Line Status Register
-uart_MSR 		EQU  0xfe ; 6 Modem Status Register
-uart_scratch 	EQU  0xff ; 7 SCR Scratch Register
+uart_tx_rx 		EQU  0x3f8 ; 0 DLAB = 0 for Regs. TX and RX
+uart_DLL 		EQU  0x3f8 ; 0 DLAB = 1 Divisor lacth low
+uart_IER 		EQU  0x3f9 ; 1 DLAB = 0 Interrupt Enable Register
+uart_DLH 		EQU  0x3f9 ; 1 DLAB = 1 Divisor lacth high
+uart_ISR 		EQU  0x3fa ; 2 IIR Interrupt Ident. Register READ ONLY
+uart_FCR 		EQU  0x3fa ; 2 Fifo Control Resgister WRITE ONLY
+uart_LCR 		EQU  0x3fb ; 3 Line Control Register
+uart_MCR 		EQU  0x3fc ; 4 Modem Control Register
+uart_LSR 		EQU  0x3fd ; 5 Line Status Register
+uart_MSR 		EQU  0x3fe ; 6 Modem Status Register
+uart_scratch 	EQU  0x3ff ; 7 SCR Scratch Register
 
 UART_FREQUENCY		equ 4915000
 ;Fomula UART_FREQUENCY/(  9600 * 16)
@@ -37,33 +37,39 @@ configure_uart:
 			mov cx, 0x4fff
 			call	basicDelay
 			MOV		AL,0x0	 		;
-			OUT  	uart_IER,	AL	; Disable interrupts
+			MOV		DX, uart_IER
+			OUT  	DX,	AL	; Disable interrupts
 
 			mov cx, 0x1f
 			call	basicDelay
 
 			MOV		AL, 0x80			;
-			OUT     uart_LCR,	AL 	; Turn DLAB on
+			MOV		DX, uart_LCR
+			OUT     DX,	AL 	; Turn DLAB on
 			mov cx, 0x1f
 			call	basicDelay
 
 			MOV		AL, UART_BAUD_38400 ;0x08
-			OUT     uart_DLL,   AL	; Set divisor low
+			MOV		DX, uart_DLL
+			OUT     DX,   AL	; Set divisor low
 			mov cx, 0x1f
 			call	basicDelay
 
 			MOV		AL, 0x00		;
-			OUT     uart_DLH,	AL	; Set divisor high
+			MOV		DX, uart_DLH
+			OUT     DX,	AL	; Set divisor high
 			mov cx, 0x1f
 			call	basicDelay
 
 			MOV     AL, 0x03	; AH	
-			OUT     uart_LCR,	AL	; Write out flow control bits 8,1,N
+			MOV		DX, uart_LCR
+			OUT     DX,	AL	; Write out flow control bits 8,1,N
 			mov cx, 0x1f
 			call	basicDelay
 
 			MOV 	AL,0x81			;
-			OUT     uart_ISR,	AL	; Turn on FIFO, with trigger level of 8.
+			MOV		DX, uart_ISR
+			OUT     DX,	AL	; Turn on FIFO, with trigger level of 8.
 								                ; This turn on the 16bytes buffer!
 			RET
 ;UART_RX:
@@ -72,11 +78,13 @@ configure_uart:
 ;			If al returns with a valid char flag carry is set, otherwise
 ;			flag carry is clear
 UART_RX:	
-			IN	AL, uart_LSR	 		; Get the line status register
+			MOV DX, uart_LSR
+			IN	AL, DX	 		; Get the line status register
 			AND AL, 0x01		; Check for characters in buffer
 			CLC 				; Clear carry
 			JZ	END				; Just ret (with carry clear) if no characters
-			IN	AL,uart_tx_rx	; Read the character from the UART receive buffer
+			MOV DX, uart_tx_rx
+			IN	AL, DX			; Read the character from the UART receive buffer
 			STC 				; Set the carry flag
 END:			
 			RET
@@ -85,7 +93,8 @@ UART_TX:
 			PUSH AX
 			MOV BX, UART_TX_WAIT	; Set CB to the transmit timeout
 LOOP_UART_TX:
-			IN	AL,	uart_LSR		; Get the line status register
+			MOV DX, uart_LSR
+			IN	AL,	DX 				; Get the line status register
 			AND AL, 0x60			; Check for TX empty
 			JNZ	OUT_UART_TX			; If set, then TX is empty, goto transmit
 			mov	cx, 0x17ff
@@ -97,7 +106,8 @@ LOOP_UART_TX:
 			RET
 OUT_UART_TX:
 			POP	AX					; Good to send at this point, so		
-			OUT	uart_tx_rx,AL		; Write the character to the UART transmit buffer
+			MOV	DX, uart_tx_rx
+			OUT	DX, AL		; Write the character to the UART transmit buffer
 			mov	cx, 0x17ff
 			call basicDelay
 			STC						; Set carry flag
@@ -110,24 +120,27 @@ print:
         	mov  al,byte ds:[bx]
         	cmp  al,0h
         	jz   fimPrint
+
+			MOV	DX, uart_tx_rx
+        	OUT	DX, AL
+			mov	cx, 0xff
+			call basicDelay
+
 cont:
-			CALL UART_TX
-			jnc cont
-        	;OUT	uart_tx_rx,AL
         	inc  bx
         	jmp  print
 fimPrint:   ret
 
 		
 
-serialLoop:
-			mov	al,'C'
-			call UART_TX
-;			mov	cx, 0xff
-;			call basicDelay
-			jmp serialLoop
-
-			ret
+;;serialLoop:
+;;			mov	al,'C'
+;;        	OUT	uart_tx_rx,AL
+;;			mov	cx, 0xff
+;;			call basicDelay
+;;			jmp serialLoop
+;;
+;;			ret
 	
 basicDelay:
         dec cx
--- src/mon86At28c64.asm
+++ src/mon86At28c64.asm
@@ -17,26 +17,38 @@
 %define	START		0E000h		; BIOS starts at offset 08000h
 %define DATE		'22/10/24'
 %define MODEL_BYTE	0FEh		; IBM PC/XT
+%define VERSION		'1.0.00'		; BIOS version
 
 bioscseg	equ	0F000h
 biosdseg	equ	0040h
 
-
+post_reg	equ	80h
+serial_timeout	equ	7Ch	; byte[4] - serial port timeout values
+equip_serial	equ	00h	; word[4] - addresses of serial ports
+unused_reg	equ	0C0h	; used for hardware detection and I/O delays
+equipment_list	equ	10h	; word - equpment list
 
         org	START
 
 init:   jmp     init2
            ;12345678901234567890
-msg0:   db "8088 - CPU TXM/8 III",0
-msg1:	db "Paulo Silva  (c)2024",0
-msg2:   db "Mon86 V 1.0.00 2410A",0
-msg3:   db "1MB dram rom at28c64",0
+msg0    db "8088 - CPU TXM/8 III",0
+msg1    db "Paulo Silva  (c)2024",0
+msg2    db "Mon86 V 1.0.00 2410A",0
+msg3    db "1MB dram rom at28c64",0
 row:    db 0, 40, 20, 84, 80
 
-msg10:  db "8088 - CPU TXM/8 III",13,10,0
-msg11:	db "Paulo Silva  (c)2024",13,10,0
-msg12:  db "Mon86 V 1.0.00 2410A",13,10,0
-msg13:  db "1MB dram rom at28c64",13,10,0
+msg10   db "8088 - CPU TXM/8 III",13,10,0
+msg11   db "Paulo Silva  (c)2024",13,10,0
+msg12   db "Mon86 V 1.0.00 2410A",13,10,0
+msg13   db "1MB dram rom at28c64",13,10,0
+
+welcome		db	"XT 8088 BIOS, Version "
+		db	VERSION
+		db	". "
+		db	"Copyright (C) 2024 - 2024 Paulo Silva(pgordao)", 0Dh, 0Ah
+		db	"8088 - CPU TXM/8 III  "
+		db	"Mon86 V 1.0.00 2410A 1MB dram rom at28c64", 0Dh, 0Ah, 0
 
 init2:
         cli				; disable interrupts
@@ -49,18 +61,10 @@ init2:
         mov es, sp
 
         call configure_uart
-
-        mov	bx,msg10
+        mov	bx,welcome
         call print
 
-        mov	bx,msg11
-        call print
 
-        mov	bx,msg12
-        call print
-
-        mov	bx,msg13
-        call print
 
 loop:
         jmp loop
@@ -129,12 +133,68 @@ byte_to_hex_str:
 .ret:
         ret
 
+;=========================================================================
+; print_digit - print hexadecimal digit
+; Input:
+;	AL - bits 3...0 - digit to print (0...F)
+; Output:
+;	none
+;-------------------------------------------------------------------------
+print_digit:
+	push	ax
+	push	bx
+	and	al,0Fh
+	add	al,'0'			; convert to ASCII
+	cmp	al,'9'			; less or equal 9?
+	jna	.1
+	add	al,'A'-'9'-1		; a hex digit
+.1:
+	mov	ah,0Eh			; Int 10 function 0Eh - teletype output
+	mov	bl,07h			; just in case we're in graphic mode
+	int	10h
+	pop	bx
+	pop	ax
+	ret
 
+;=========================================================================
+; print_hex - print 16-bit number in hexadecimal
+; Input:
+;	AX - number to print
+; Output:
+;	none
+;-------------------------------------------------------------------------
+print_hex:
+	xchg	al,ah
+	call	print_byte		; print the upper byte
+	xchg	al,ah
+	call	print_byte		; print the lower byte
+	ret
+;=========================================================================
+; print_byte - print a byte in hexadecimal
+; Input:
+;	AL - byte to print
+; Output:
+;	none
+;-------------------------------------------------------------------------
+print_byte:
+	rol	al,1
+	rol	al,1
+	rol	al,1
+	rol	al,1
+	call	print_digit
+	rol	al,1
+	rol	al,1
+	rol	al,1
+	rol	al,1
+	call	print_digit
+	ret
 
 %include "DRV16C550_8088.asm"		
 %include "DRVLCD20X04_8088.asm"	
-%include "serial1.inc"	
-%include "serial2.inc"	
+;%include "serial1.inc"	
+;%include "serial2.inc"	
+;%include "errno.inc"	
+;%include "messages.inc"	
 
         setloc	0FFF0h			; Power-On Entry Point
 reset:
